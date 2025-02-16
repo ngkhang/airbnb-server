e02ac1b5a56a00e435e330db8d54f010239e1e58
Khang Nguyen <ngkhang0220@gmail.com>
2025-02-16 22:11:53 +0700

    chore(setup): add Prettier and config rules for Prettier and ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import eslint from '@eslint/js';
+import * as importPlugin from 'eslint-plugin-import';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -24,12 +25,108 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx']
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json'
+        },
+        node: true
+      }
+    },
   },
   {
     rules: {
+      // --- typescript-eslint rules ---
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      "@typescript-eslint/consistent-type-assertions": ["error", { "assertionStyle": "angle-bracket" }],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        { "selector": "default", "format": ["strictCamelCase"] },
+        { "selector": "variable", "format": ["strictCamelCase", "UPPER_CASE", "StrictPascalCase"] },
+        // https://github.com/microsoft/TypeScript/issues/9458
+        { "selector": "parameter", "modifiers": ["unused"], "format": ["strictCamelCase"], "leadingUnderscore": "allow" },
+        { "selector": "property", "format": null },
+        { "selector": "typeProperty", "format": null },
+        { "selector": "typeLike", "format": ["StrictPascalCase"] },
+        { "selector": "enumMember", "format": ["UPPER_CASE"] }
+      ],
+      "@typescript-eslint/no-extraneous-class": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/restrict-template-expressions": ["error", { "allowNumber": true }],
+      // --- eslint rules ---
+      'arrow-body-style': [
+        'error',
+        'as-needed',
+        {
+          'requireReturnForObjectLiteral': true
+        }
+      ],
+      'semi': 'error',
+      'no-console': 'error',
+      'no-nested-ternary': 'error',
+      'no-lonely-if': 'warn',
+      'no-undefined': 'warn',
+      'no-unexpected-multiline': 'warn',
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'warn',
+      // --- prettier rules ---
+      'prettier/prettier': [
+        'error',
+        {
+          'endOfLine': 'auto'
+        }
+      ],
+      // --- import rules ---
+      "import/named": "off",
+      "import/no-default-export": "error",
+      "import/prefer-default-export": "off",
+      'import/order': [
+        'error',
+        {
+          'groups': [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type'
+          ],
+          'newlines-between': 'always',
+          'alphabetize': {
+            'order': 'asc',
+            'caseInsensitive': true
+          },
+          'pathGroups': [
+            {
+              'pattern': '@nestjs/**',
+              'group': 'external',
+              'position': 'before'
+            }
+          ]
+        }
+      ],
     },
   },
 );
